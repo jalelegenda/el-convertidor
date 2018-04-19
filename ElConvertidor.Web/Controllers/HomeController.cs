04e03d85d697b03fd22366756d91469ba271ea64
Jakov Milic <jakov.milic@emovis.hr>
2018-04-19 16:08:25 +0200

    Convert single image file to .tiff format

--- ElConvertidor.Web/Controllers/HomeController.cs
+++ ElConvertidor.Web/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using ElConvertidor.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +13,29 @@ namespace ElConvertidor.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult UploadImages()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult UploadImages(HttpPostedFileBase file)
+        {
+            var tiffEncoder = ImageCodecInfo.GetImageEncoders()
+                .SingleOrDefault(dec => dec.FormatID == ImageFormat.Tiff.Guid);
+            Stream imgStream = new MemoryStream();
+            using (EncoderParameters encParams = new EncoderParameters(1))
+            using (Image img = Image.FromStream(file.InputStream, true, true))
+            {
+                encParams.Param[0] = new EncoderParameter(Encoder.Quality, 100);
+                img.Save(imgStream, tiffEncoder, encParams);
+            }
+            imgStream.Position = 0;
+            var result = File(imgStream, "image/tiff", "image.tif");
+            return result;
         }
     }
 }
---
+++ ElConvertidor.Web/Models/UploadedFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElConvertidor.Web.Models
+{
+    public class UploadedFile
+    {
+        public HttpPostedFileBase Image { get; set; }
+    }
+}
