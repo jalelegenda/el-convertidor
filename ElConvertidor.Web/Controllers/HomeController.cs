2583b963b91f520b978efbf6e0f9dac98a0e5dc9
Jakov Milic <jakov.milic@emovis.hr>
2018-04-22 22:14:52 +0200

    Convert multiple images to single multipage tif

--- ElConvertidor.Web/Controllers/HomeController.cs
+++ ElConvertidor.Web/Controllers/HomeController.cs
@@ -22,20 +22,44 @@ namespace ElConvertidor.Web.Controllers
 
         [HttpPost]
         [ActionName("Index")]
-        public ActionResult UploadImages(HttpPostedFileBase file)
+        public ActionResult UploadImages(IEnumerable<HttpPostedFileBase> files)
         {
-            var tiffEncoder = ImageCodecInfo.GetImageEncoders()
-                .SingleOrDefault(dec => dec.FormatID == ImageFormat.Tiff.Guid);
-            Stream imgStream = new MemoryStream();
-            using (EncoderParameters encParams = new EncoderParameters(1))
-            using (Image img = Image.FromStream(file.InputStream, true, true))
+            if (!ModelState.IsValid)
             {
-                encParams.Param[0] = new EncoderParameter(Encoder.Quality, 100);
-                img.Save(imgStream, tiffEncoder, encParams);
+                return View();
             }
-            imgStream.Position = 0;
-            var result = File(imgStream, "image/tiff", "image.tif");
-            return result;
+
+            Stream imageStream = new MemoryStream();
+
+            using (EncoderParameters encParams = new EncoderParameters(1))
+            using (EncoderParameter multiParam = new EncoderParameter(Encoder.SaveFlag, (long)EncoderValue.MultiFrame))
+            using (EncoderParameter pageParam = new EncoderParameter(Encoder.SaveFlag, (long)EncoderValue.FrameDimensionPage))
+            using (EncoderParameter flushParam = new EncoderParameter(Encoder.SaveFlag, (long)EncoderValue.Flush))
+            {
+                var tiffEncoderInfo = ImageCodecInfo.GetImageEncoders()
+                    .FirstOrDefault(dec => dec.FormatID == ImageFormat.Tiff.Guid);
+
+                Queue<HttpPostedFileBase> fileQueue = new Queue<HttpPostedFileBase>(files);
+                encParams.Param[0] = multiParam;
+
+                var file = fileQueue.Dequeue();
+
+                Image mainTiff = Image.FromStream(file.InputStream, true, true);
+                mainTiff.Save(imageStream, tiffEncoderInfo, encParams);
+
+                while (fileQueue.Count > 0)
+                {
+                    encParams.Param[0] = pageParam;
+
+                    file = fileQueue.Dequeue();
+                    Image page = Image.FromStream(file.InputStream, true, true);
+                    mainTiff.SaveAdd(page, encParams);
+                }
+                encParams.Param[0] = flushParam;
+                imageStream.Position = 0;
+            }
+
+            return File(imageStream, "image/tiff", "image.tif");
         }
     }
 }
